86553f2eb8fbbe283b32ce53d7ca60b87b90daa4
Isaac0424 <eodud3858@gmail.com>
2026-09-10 17:51:31 +0900

    feat. 수업 종료로 인한 임시 저장

--- 20260910/streamlit_fastapi_chat_proj/proj_streamlit/app.py
+++ 20260910/streamlit_fastapi_chat_proj/proj_streamlit/app.py
@@ -5,6 +5,22 @@ FASTAPI_URL = "http://127.0.0.1:8000"
 
 st. title("Streamlit & FastAPI Chat Example")
 
+with st.form("add_user_name"):
+    name = st.text_input("이름", value = "홍길동")
+    submit_button = st.form_submit_button("백엔드로 전송")
+
+if submit_button:
+    payload = {
+        "name": name
+    }
+    
+    try:
+        response = requests.post(f"{FASTAPI_URL}")
+        pass
+    except:
+        pass
 prompt = st.chat_input("Say something")
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+    response = requests.post(f"{FASTAPI_URL}/send_messages", json=prompt)
+    st.write(f"User: {prompt}")
+    
